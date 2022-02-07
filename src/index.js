a0c0ca7ac335de1f4d2409aa907e1ba3aa0f91a0
Tom Alder <tom@alder.cx>
2022-02-07 14:11:47 +1100

    Call entire array in query q, sort data client side. Working for most views, comments, newest, oldest.

--- src/index.js
+++ src/index.js
@@ -39,11 +39,16 @@ const firebaseConfig = {
   measurementId: "G-ZDJ82ELVP8"
 };
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 // init firebase app
-initializeApp(firebaseConfig)
+// initializeApp(firebaseConfig)
+
+// const q = query(collection(db, "users"));
 
 // init services
-const db = getFirestore()
+// const db = getFirestore()
 
 // collection ref - CHANGE THIS TO CHANGE THE FIRESTORE COLLECTION YOU PULL FROM
 const colRef = collection(db, 'posts-g-sheets-6');
@@ -86,14 +91,244 @@ const container = document.querySelector('.containerload');
 // Store last document
 let latestDoc = null;
 
+
+// // // CLIENT SORT // // //
+
 // Set initial query
-var currentQuery = query(colRef, orderBy('numViews', 'asc'), startAfter(latestDoc), limit(6));
-var orderCol = 'numViews';
+var currentFirstQuery = query(q, orderBy('createdAt', 'desc'), limit(6));
+var currentNextQuery = query(q, orderBy('createdAt', 'desc'), startAfter(latestDoc), limit(6));
+var orderCol = 'createdAt';
+var orderDir = 'desc'
 
 // function to update query
-function recalcQuery () {
-  currentQuery = query(colRef, orderBy(orderCol, 'asc'), startAfter(latestDoc), limit(6));
+function recalcFirstQuery () {
+  currentFirstQuery = query(q, orderBy(orderCol, orderDir), limit(6));
 }
+
+function recalcNextQuery () {
+  currentNextQuery = query(q, orderBy(orderCol, orderDir), startAfter(latestDoc), limit(6));
+}
+
+const getFirstReviews = async () => {
+    console.log('getFirstReviews has run!')
+
+    recalcFirstQuery();
+    var load = currentFirstQuery;
+
+    // Retrieve the first 5 data
+    const data = await getDocs(load);
+
+    // To output the retrieved first 5 data
+    data.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log("Summary: ", doc.data().summary , " => ", doc.data().createdAt);
+        // use toDate to easily read dates
+        console.log("Summary: ", doc.data().summary , " => ", doc.data().createdAt.toDate());
+    });
+
+    // Run renderPosts based on value of data
+    renderPosts(data);
+
+    // Update latestDoc reference 
+    latestDoc =  data.docs[data.docs.length-1]
+
+    // // Unattach event listeners if no more documents
+    if (data.empty) {
+      loadMore.removeEventListener('click', handleClick)
+    }
+}
+
+const getNextReviews = async () => {
+    console.log('getNextReviews has run!')
+
+    recalcNextQuery();
+    var next = currentNextQuery;
+        
+    // Automatically pulls the remaining data in the collection
+    const data_next = await getDocs(next);
+        
+    // Outputs the remaining data in the collection
+    data_next.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log("Summary: ", doc.data().summary , " => ", doc.data().createdAt);
+        // use toDate to easily read dates
+        console.log("Summary: ", doc.data().summary , " => ", doc.data().createdAt.toDate());
+    });
+
+    // Run renderPosts based on value of data
+    renderPosts(data_next);
+
+    // Update latestDoc reference 
+    latestDoc =  data_next.docs[data_next.docs.length-1]
+
+    // // Unattach event listeners if no more documents
+    if (data_next.empty) {
+      loadMore.removeEventListener('click', handleClick)
+    }
+}
+
+function renderPosts (data) {
+  loadAfterSecondTime();
+  let template = '';
+  data.docs.forEach(doc => {
+    const grabData = doc.data();
+    template += `
+    <div class="mix company-${grabData.company} blog-card" data-ref="item">
+      <div class="linkedin-post">
+        <div class="card-preloader" id="card-preloader">
+          <div class="card-loader card-loader--tabs">
+            <div class="skeleton-content-coumn">
+              <div class="skeleton-header-line">
+                <div class="skeleton-type-icon">
+                </div>  
+                <div class="skeleton-title">
+                </div>
+              </div>
+              <div class="skeleton-content-1"></div>
+              <div class="skeleton-content-2"></div>
+              <div class="skeleton-content-3"></div>
+              <div class="skeleton-content-4"></div>
+
+              <div class="skeleton-content-6"></div>
+              <div class="skeleton-content-7"></div>
+              <div class="skeleton-content-8"></div>
+            </div>
+          </div>
+        </div>
+        <iframe src="https://www.linkedin.com/embed/feed/update/${grabData.embedlink}" height="420" width="500" frameborder="0" allowfullscreen="" title="Embedded post" loading="lazy" class="iframe" style="opacity: 0">
+        </iframe>
+        <div class="card-border">
+        </div>
+      </div>
+    </div> 
+    `
+  });
+  container.innerHTML += template;
+}
+
+// // // END CLIENT SORT
+
+
+// // Set initial query
+// var currentQuery = query(q, orderBy('numViews', 'asc'), startAfter(latestDoc), limit(6));
+// var orderCol = 'numViews';
+
+// // function to update query
+// function recalcQuery () {
+//   currentQuery = query(q, orderBy(orderCol, 'asc'), startAfter(latestDoc), limit(6));
+// }
+
+
+// // // *--- SORT BY ---* // // //
+
+// Sort by least Views WORKING (NOT IN USE)
+function leastViews() {
+  console.log('leastViews has run');
+  orderCol = 'numViews';
+  orderDir = 'desc';
+  clear();
+  getNextPosts();
+}
+
+// Sort by most Views WORKING
+function mostViews() {
+  console.log('mostViews has run');
+  orderCol = 'numViews';
+  orderDir = 'desc';
+  clear();
+  getFirstReviews();
+}
+// Sort by most Comments WORKING
+function mostComments() {
+  console.log('mostComments has run');
+  orderCol = 'numComments';
+  orderDir = 'desc';
+  clear();
+  getFirstReviews();
+}
+
+// Sort by most Recent BUILDING
+function mostRecent() {
+  console.log('mostRecent has run');
+  orderCol = 'createdAt';
+  orderDir = 'desc';
+  clear();
+  getFirstReviews();
+}
+
+// Sort by least Recent BUILDING
+function leastRecent() {
+  console.log('leastRecent has run');
+  orderCol = 'createdAt';
+  orderDir = 'asc';
+  clear();
+  getFirstReviews();
+}
+
+// Clear latest doc and html container
+function clear() {
+  container.innerHTML = '';
+  latestDoc = null;
+}
+
+// // // *--- SORT AND FILTER TOGGLES ---* // // //
+
+// Most Views (dropdown)
+var mostViewsDropdown = document.getElementById('mostViewsID');
+mostViewsDropdown.onclick = function() {
+    console.log('Sort by most Views');
+    mostViews();
+}
+
+// Most comments (dropdown)
+var mostCommentsDropdown = document.getElementById('mostCommentsID');
+mostCommentsDropdown.onclick = function() {
+    console.log('Sort by most Comments');
+    mostComments();
+}
+
+// Most Recent (dropdown)
+var mostRecentDropdown = document.getElementById('mostRecentID');
+mostRecentDropdown.onclick = function() {
+    console.log('Sort by most Recent');
+    mostRecent();
+}
+
+// Least Recent (dropdown)
+var leastRecentDropdown = document.getElementById('leastRecentID');
+leastRecentDropdown.onclick = function() {
+    console.log('Sort by least Recent');
+    leastRecent();
+}
+
+// Filter Linkedin (dropdown) NOT IN USE
+var filterLinkedInDropdown = document.getElementById('filterLinkedInID');
+filterLinkedInDropdown.onclick = function() {
+    console.log('Filter by LinkedIn');
+    filterLinkedIn();
+}
+
+// Load more docs (button)
+const loadMore = document.querySelector('.load-more button');
+
+const handleClick = () => {
+  console.log(latestDoc);
+  console.log('^ on load more button');
+  getNextReviews();
+}
+
+// For some reason this breaks grid page...
+loadMore.addEventListener('click', handleClick);
+
+// wait for DOM content to load
+window.addEventListener('DOMContentLoaded', () => getFirstReviews());
+// window.addEventListener('DOMContentLoaded', () => leastViews());
+
+
+// // //
+// // // *--- NOT IN USE ---* // // //
+// // //
+
 
 // Get next posts
 const getNextPosts = async () => {
@@ -167,103 +402,6 @@ const getNextPosts = async () => {
   }
 }
 
-// // // *--- SORT BY ---* // // //
-
-// Sort by least Views WORKING
-function leastViews() {
-  console.log('leastViews has run');
-  orderCol = 'numViews';
-  clear();
-  getNextPosts();
-}
-
-// Sort by most Views WORKING
-function mostViews() {
-  console.log('mostViews has run');
-  orderCol = 'numLikesNeg';
-  clear();
-  getNextPosts();
-}
-// Sort by most Comments WORKING
-function mostComments() {
-  console.log('mostComments has run');
-  orderCol = 'numCommentsNeg';
-  clear();
-  getNextPosts();
-}
-
-// Sort by most Comments WORKING
-// function mostComments() {
-//   console.log('mostComments has run');
-//   orderCol = 'numCommentsNeg';
-//   clear();
-//   getNextPosts();
-// }
-
-// Sort by most Recent BUILDING
-function mostRecent() {
-  console.log('mostRecent has run');
-  orderCol = 'createdAt';
-  clear();
-  getNextPosts();
-}
-
-// Clear latest doc and html container
-function clear() {
-  container.innerHTML = '';
-  latestDoc = null;
-}
-
-// // // *--- SORT AND FILTER TOGGLES ---* // // //
-
-// Filter Linkedin (dropdown)
-var filterLinkedInDropdown = document.getElementById('filterLinkedInID');
-filterLinkedInDropdown.onclick = function() {
-    console.log('Filter by LinkedIn');
-    filterLinkedIn();
-}
-
-// Most Views (dropdown)
-var mostViewsDropdown = document.getElementById('mostViewsID');
-mostViewsDropdown.onclick = function() {
-    console.log('Sort by most Views');
-    mostViews();
-}
-
-// Most comments (dropdown)
-var mostCommentsDropdown = document.getElementById('mostCommentsID');
-mostCommentsDropdown.onclick = function() {
-    console.log('Sort by most Comments');
-    mostComments();
-}
-
-// Most Recent (dropdown)
-var mostRecentDropdown = document.getElementById('mostRecentID');
-mostRecentDropdown.onclick = function() {
-    console.log('Sort by most Recent');
-    mostRecent();
-}
-
-// Load more docs (button)
-const loadMore = document.querySelector('.load-more button');
-
-const handleClick = () => {
-  console.log(latestDoc);
-  console.log('^ on load more button');
-  getNextPosts();
-}
-
-// For some reason this breaks grid page...
-loadMore.addEventListener('click', handleClick);
-
-// wait for DOM content to load
-window.addEventListener('DOMContentLoaded', () => leastViews());
-
-
-// // //
-// // // *--- NOT IN USE ---* // // //
-// // //
-
 
 
 // // // *--- FUNCTIONS TO RUN QUERIES ---* // // //
@@ -278,7 +416,7 @@ onSnapshot(q, (snapshot) => {
     })
   })
 
-  console.log(posts)
+  // console.log(posts)
   document.getElementById('postcard-mixitup').innerHTML = `
     ${posts.map(function(grabData) {
       return `
