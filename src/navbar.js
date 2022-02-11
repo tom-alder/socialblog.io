54bac0a466a78ae539440834751f7c8b7d1c68c3
Tom Alder <tom@alder.cx>
2022-02-11 13:18:28 +1100

    Add postcards to template.html - Note Boostratp in html header which will be changed to Tailwind

--- src/navbar.js
+++ src/navbar.js
@@ -23,18 +23,6 @@
 		init();
 	}();
 
-	
-	//Switch light/dark
-	
-	$("#switch").on('click', function () {
-		if ($("body").hasClass("light")) {
-			$("body").removeClass("light");
-			$("#switch").removeClass("switched");
-		}
-		else {
-			$("body").addClass("light");
-			$("#switch").addClass("switched");
-		}
-	});
+
 	
 })(jQuery); 
